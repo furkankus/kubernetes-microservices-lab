08cac75f8650bf111bc704be8d31795c04ba62a5
Furkan Kuş <furkankus@gmail.com>
2026-02-14 18:55:34 +0300

    Add Tempo installation to observability script

--- observability/install.sh
+++ observability/install.sh
@@ -2,3 +2,7 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm upgrade --install loki grafana/loki-stack \
   -n monitoring \
   -f helm-values/loki-stack.yaml
+
+helm upgrade --install tempo grafana/tempo \
+  -n monitoring \
+  -f helm-values/tempo.yaml
